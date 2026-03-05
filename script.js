9be3660ef5aaec7876d4004db38a8a0e383b1861
Jatinn-D <jd0287@srmist.edu.in>
2026-03-05 19:29:11 +0530

    pushing my gsap learnings

---
+++ script.js
@@ -0,0 +1,21 @@
+var tl = gsap.timeline();
+
+tl.from("h2",{
+  y: -30,
+  opacity: 0,
+  duration: 0.8,
+  delay: 0.5,
+})
+
+tl.from("h4",{
+  y: -30,
+  opacity: 0,
+  duration: 0.8,
+  stagger: 0.3,
+})
+
+tl.from("h1",{
+  y: 20,
+  opacity: 0,
+  duration: 0.8,
+})
